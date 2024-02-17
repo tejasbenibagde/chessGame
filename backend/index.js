fcde7323802617864bdebe51541e24bbb079b79d
tejasbenibagde <124677750+tejasbenibagde@users.noreply.github.com>
2024-02-17 15:13:04 +0530

    First Commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(5001, () => {
+  console.log("server started at port 5001");
+});
