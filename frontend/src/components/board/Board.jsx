b84e213daac305eb304152c7f4e7ae26163fa43c
tejasbenibagde <124677750+tejasbenibagde@users.noreply.github.com>
2024-02-25 17:10:02 +0530

    Pieces Now Snaps to the boxes

--- frontend/src/components/board/Board.jsx
+++ frontend/src/components/board/Board.jsx
@@ -1,68 +1,115 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Tile from "../tiles/Tile";
 
 function Board() {
+  const [activePiece, setActivePiece] = useState(null);
+  const [gridX, setGridX] = useState(0);
+  const [gridY, setGridY] = useState(0);
+  const chessBoardRef = useRef(null);
   const board = [];
-  const pieces = [];
+  // const pieces = [];
   const xAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
   const yAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+
+  const initialBoardState = [];
 
   // Pawns
   for (let i = 0; i < 8; i++) {
     // Black
-    pieces.push({ image: "pieces/B_pawn.png", x: i, y: 6 });
+    initialBoardState.push({ image: "pieces/B_pawn.png", x: i, y: 6 });
   }
   for (let i = 0; i < 8; i++) {
     // White
-    pieces.push({ image: "pieces/W_pawn.png", x: i, y: 1 });
+    initialBoardState.push({ image: "pieces/W_pawn.png", x: i, y: 1 });
   }
 
   for (let i = 0; i <= 1; i++) {
     let color = i === 0 ? "W" : "B";
     let y = color === "W" ? 0 : 7;
     // Rooks
-    pieces.push({ image: `pieces/${color}_rook.png`, x: 0, y });
-    pieces.push({ image: `pieces/${color}_rook.png`, x: 7, y });
+    initialBoardState.push({ image: `pieces/${color}_rook.png`, x: 0, y });
+    initialBoardState.push({ image: `pieces/${color}_rook.png`, x: 7, y });
     // Knights
-    pieces.push({ image: `pieces/${color}_knight.png`, x: 1, y });
-    pieces.push({ image: `pieces/${color}_knight.png`, x: 6, y });
+    initialBoardState.push({ image: `pieces/${color}_knight.png`, x: 1, y });
+    initialBoardState.push({ image: `pieces/${color}_knight.png`, x: 6, y });
     // Bishops
-    pieces.push({ image: `pieces/${color}_bishop.png`, x: 2, y });
-    pieces.push({ image: `pieces/${color}_bishop.png`, x: 5, y });
+    initialBoardState.push({ image: `pieces/${color}_bishop.png`, x: 2, y });
+    initialBoardState.push({ image: `pieces/${color}_bishop.png`, x: 5, y });
     // Queen
-    pieces.push({ image: `pieces/${color}_queen.png`, x: 3, y });
+    initialBoardState.push({ image: `pieces/${color}_queen.png`, x: 3, y });
     // King
-    pieces.push({ image: `pieces/${color}_king.png`, x: 4, y });
+    initialBoardState.push({ image: `pieces/${color}_king.png`, x: 4, y });
   }
-
-  let activePiece = null;
+  const [pieces, setPieces] = useState(initialBoardState);
 
   function grabbingPiece(e) {
-    if (e.target.classList.contains("piece")) {
+    const element = e.target;
+    const chessBoard = chessBoardRef.current;
+    if (element.classList.contains("piece") && chessBoard) {
+      setGridX(Math.floor((e.clientX - chessBoard.offsetLeft) / 80));
+      setGridY(
+        Math.abs(Math.ceil((e.clientY - chessBoard.offsetTop - 640) / 80))
+      );
       const x = e.clientX - 40;
       const y = e.clientY - 40;
-      e.target.style.position = " absolute";
-      e.target.style.left = `${x}px`;
-      e.target.style.top = `${y}px`;
+      element.style.position = " absolute";
+      element.style.left = `${x}px`;
+      element.style.top = `${y}px`;
 
-      activePiece = e.target;
+      setActivePiece(element);
     }
   }
 
   function movePiece(e) {
-    const element = e.target;
-    if (activePiece && activePiece.classList.contains("piece")) {
+    const chessBoard = chessBoardRef.current;
+    if (activePiece && chessBoard && activePiece.classList.contains("piece")) {
+      const minX = chessBoard.offsetLeft;
+      const minY = chessBoard.offsetTop;
+      const maxX = minX + chessBoard.clientWidth - 70;
+      const maxY = minY + chessBoard.clientHeight - 70;
       const x = e.clientX - 40;
       const y = e.clientY - 40;
       activePiece.style.position = " absolute";
-      activePiece.style.left = `${x}px`;
-      activePiece.style.top = `${y}px`;
+
+      if (x < minX) {
+        activePiece.style.left = `${minX}px`;
+      } else if (x > maxX) {
+        activePiece.style.left = `${maxX}px`;
+      } else {
+        activePiece.style.left = `${x}px`;
+      }
+
+      if (y < minY) {
+        activePiece.style.top = `${minY}px`;
+      } else if (y > maxY) {
+        activePiece.style.top = `${maxY}px`;
+      } else {
+        activePiece.style.top = `${y}px`;
+      }
     }
   }
 
-  function dropPiece(e){
-    if(activePiece){
-      activePiece = null;
+  function dropPiece(e) {
+    const chessBoard = chessBoardRef.current;
+    if (activePiece && chessBoard) {
+      const x = Math.floor((e.clientX - chessBoard.offsetLeft) / 80);
+      const y = Math.abs(
+        Math.ceil((e.clientY - chessBoard.offsetTop - 640) / 80)
+      );
+
+      // console.log("clientX = " + e.clientX + "clientY = " + e.clientY);
+      // console.log(x, y);
+      setPieces((value) => {
+        const pieces = value.map((p) => {
+          if (p.x === gridX && p.y === gridY) {
+            p.x = x;
+            p.y = y;
+          }
+          return p;
+        });
+        return pieces;
+      });
+      setActivePiece(null);
     }
   }
 
@@ -87,10 +134,11 @@ function Board() {
   }
   return (
     <div
+      ref={chessBoardRef}
       onMouseDown={(e) => grabbingPiece(e)}
       onMouseMove={(e) => movePiece(e)}
       onMouseUp={(e) => dropPiece(e)}
-      className="h-[80vh] w-[80vh] bg-blue-300 flex flex-wrap"
+      className="h-[640px] w-[640px] bg-blue-300 flex flex-wrap"
     >
       {board}
     </div>
--- frontend/src/components/tiles/Tile.jsx
+++ frontend/src/components/tiles/Tile.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function Tile({ color, image }) {
   return (
-    <div className="w-[10vh] h-[10vh]" style={{ backgroundColor: color }}>
+    <div className="w-[80px] h-[80px]" style={{ backgroundColor: color }}>
       {image && (
         <div
           style={{ backgroundImage: `url(${image})` }}
           className={`${
             image && "piece"
-          } w-[10vh] h-[10vh] bg-no-repeat hover:cursor-grab active:cursor-grabbing`}
+          } w-[80px] h-[80px] bg-no-repeat hover:cursor-grab active:cursor-grabbing`}
         ></div>
       )}
     </div>
